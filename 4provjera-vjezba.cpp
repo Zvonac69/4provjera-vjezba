fa30c941c638f60541bb05e5068f799c024a15ea
Zvonac69 <64528559+Zvonac69@users.noreply.github.com>
2020-05-03 22:50:18 +0200

    Update 4provjera-vjezba.cpp
    1.zadatak,sluzi za unos broja računa,prezimena i imena i stanja računa

--- 4provjera-vjezba.cpp
+++ 4provjera-vjezba.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
-
+#include<iostream>
+#include<algorithm>
+#include<numeric>
 using namespace std;
 
-int main()
+void NewRac(unsigned long long brRacuna[], string ime[], double stnr[], int brKlijenata)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    unsigned long long in;
+    while(1)
+    {
+        try
+        {
+            cout << "Unesite broj racuna: ";
+            cin >> in;
+            if(in < 1000000000 || in > 9999999999)
+            {
+                throw "Neispravan unos";
+            }
+            break;
+        }
+        catch(const char *x)
+        {
+            cout << x << endl;
+        }
+    }
+    brRacuna[brKlijenata] = in;
+    cout << "Unesite prezime i ime: ";
+    cin.ignore();
+    getline(cin, ime[brKlijenata]);
+    cout<<"Unesite stanje na racunu: ";
+    cin>>stnr[brKlijenata];
 }
+
